1da8a4901d7993b90e19a5d1f01ed4833616e157
Tobias <tobiashagnessmortensen@gmail.com>
2023-11-07 23:49:48 +0100

    done with step 1-7

--- sbom.py
+++ sbom.py
@@ -4,36 +4,51 @@ import os
 from pathlib import Path
 import sys
 
-def extract_info(repo_dir: str | Path) -> None | dict:
+def extract_info(filename: str | Path, repo_dir: str | Path) -> None | dict:
     """Finding the correct files to read from and creating dicts for the information
         If no valid file is found, returning None
 
     Args:
-        repo_dir (str | Path): Path to the source code repositories
+        filename (str | Path): path to the filename of interest
+        repo_dir (str | Path): path to the source code repositories
 
     Returns:
         None | dict: If
     """
     repo_dir = Path(repo_dir)
     #Check for requirements.txt for Python projects and extracts the info wanted
-    if os.path.exists(os.path.join(repo_dir, 'requirements.txt')):
-        with open(os.path.join(repo_dir, 'requirements.txt'), 'r') as txtfile:
-            content = txtfile.read()
+    if filename == "requirements.txt":
+        with open(os.path.join(repo_dir, filename), "r") as txtfile:
+            content = txtfile.read().split("==")
             return {
-                "name": str(repo_dir),
-                "version": "",
+                "name": content[0],
+                "version": content[1],
+                "description": "",
                 "type": "pip",
-                "path": os.path.abspath(os.path.join(repo_dir, 'requirements.txt'))
+                "path": os.path.abspath(os.path.join(repo_dir, filename))
             }
     #Check for package.json for JavaScript projects and extracts the info wanted
-    elif os.path.exists(os.path.join(repo_dir, 'package.json')):
-        with open(os.path.join(repo_dir, 'package.json'), 'r') as jsonfile:
+    elif filename == "package.json":
+        with open(os.path.join(repo_dir, filename), "r") as jsonfile:
             content = json.load(jsonfile)
             return {
                 "name": content["name"],
                 "version": content["version"],
+                "description": content["description"],
+                "type": "npm: " + content["engines"]["npm"],
+                "path": os.path.abspath(os.path.join(repo_dir, filename)),
+                "license": content["license"]
+            }
+    #Check for package-lock.json for JavaScript projects and extracts the info wanted
+    elif filename == "package-lock.json":
+        with open(os.path.join(repo_dir, filename), "r") as lockfile:
+            content = json.load(lockfile)
+            return {
+                "name": content["name"],
+                "version": content["version"],
+                "description": "",
                 "type": "npm",
-                "path": os.path.abspath(os.path.join(repo_dir, 'package.json'))
+                "path": os.path.abspath(os.path.join(repo_dir, filename))
             }
     #Returns None if none of the valid files were found
     else:
@@ -45,15 +60,15 @@ def save_as_CSV(sbom_data: list, parent_dir: str | Path) -> None:
 
     Args:
         sbom_data (list): list of dicts to read from
-        parent_dir (str | Path): Path to directory for saveing the CSV-file
+        parent_dir (str | Path): path to directory for saveing the CSV-file
     
     Returns:
         None
     """
     parent_dir = Path(parent_dir)
-    csv_file = os.path.join(parent_dir, 'sbom.csv')
-    with open(csv_file, 'w', newline='') as csvfile:
-        fieldnames = ['name', 'version', 'type', 'path']
+    csv_file = os.path.join(parent_dir, "sbom.csv")
+    with open(csv_file, "w", newline="") as csvfile:
+        fieldnames = ["name", "version", "description", "type", "path", "license"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for item in sbom_data:
@@ -66,20 +81,20 @@ def save_as_JSON(sbom_data: dict, parent_dir: str | Path) -> None:
 
     Args:
         sbom_data (list): list of dicts to read from
-        parent_dir (str | Path): Path to directory for saveing the JSON-file
+        parent_dir (str | Path): path to directory for saveing the JSON-file
     
     Returns:
         None
     """
     parent_dir = Path(parent_dir)
-    json_file = os.path.join(parent_dir, 'sbom.json')
-    with open(json_file, 'w') as jsonfile:
+    json_file = os.path.join(parent_dir, "sbom.json")
+    with open(json_file, "w") as jsonfile:
         json.dump(sbom_data, jsonfile, indent=4)
     print(f"Saved SBOM in JSON format to '{parent_dir}'")
 
 
 def create_sbom(directory: str | Path) -> None:
-    """
+    """Creates the list of sbom_data and then calls the different functions to save as different files
 
     Args:
         directory (str | Path): path to the passing directory
@@ -98,14 +113,18 @@ def create_sbom(directory: str | Path) -> None:
     
     #iterates through the subdirectories calls on extract_info to get the data from each valid file
     sbom_data = []
+    
     for subrepo in directory.iterdir():
         if subrepo.is_dir():
-            info = extract_info(subrepo)
-            if info:
-                sbom_data.append(info)
-            else: 
-                print(f"Your repo: '{full_repos_path / subrepo.name}' does not have any of the targeted files. Targeted files is eiter 'requirements.txt' or 'package.json'.")
-                sys.exit(1)
+            for item in subrepo.iterdir():
+                if item.suffix != "":
+                    filename = item.stem + item.suffix
+                    info = extract_info(filename, subrepo)
+                    if info:
+                        sbom_data.append(info)
+                    else: 
+                        print(f"Your repo: '{full_repos_path / subrepo.name}' does not have any of the targeted files. Targeted files is eiter 'requirements.txt' or 'package.json'.")
+                        sys.exit(1)
 
     #saves the sbom_data to CSV and JSON file
     save_as_CSV(sbom_data, full_repos_path)
